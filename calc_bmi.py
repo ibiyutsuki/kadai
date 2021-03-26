77a0a4f5b18f823e7f3a42568c87de9ee423f3c9
ibi yutsuki <y.ibi.sys20@morijyobi.ac.jp>
2021-03-26 11:32:09 +0900

    練習問題

---
+++ calc_bmi.py
@@ -0,0 +1,6 @@
+import main
+
+si = float(input("身長:"))
+ta = float(input("体重:")) / 100
+
+print(f"BMI:{main.get_area(si,ta)}")
---
+++ main.py
@@ -0,0 +1,3 @@
+def get_area(sintyo,taijyu):
+    BMI =  taijyu / sintyo / sintyo 
+    return BMI
